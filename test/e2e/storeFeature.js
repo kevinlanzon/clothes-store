7072daa1f90d3879b4f66744b1d4e38840148ddf
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-24 14:30:28 +0100

    adds product search box

--- test/e2e/storeFeature.js
+++ test/e2e/storeFeature.js
@@ -1,7 +1,9 @@
 describe('Homepage', function() {
 
-  browser.get('index.html');
-  productList = element.all(by.tagName('h5'));
+  beforeEach(function() {
+    browser.get('index.html');
+    productList = element.all(by.tagName('h5'));
+  });
 
   it('has a title', function() {
     expect(browser.getTitle()).toEqual('Clothes Store');
@@ -9,5 +11,17 @@ describe('Homepage', function() {
 
   it('displays 13 products', function() {
     expect(productList.count()).toBe(13);
+  });
+
+  it('should filter the product list as a user types into the search box', function() {
+    var productList = element.all(by.repeater('product in products'));
+    var query = element(by.model('query'));
+    expect(productList.count()).toBe(13);
+    query.sendKeys('Suede Shoes');
+    expect(productList.count()).toBe(1);
+
+    query.clear();
+    query.sendKeys('Flip Flops');
+    expect(productList.count()).toBe(2);
   });
 });
