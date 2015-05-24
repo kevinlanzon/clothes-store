1e604388f8ff7f153bf632ee84eccf8947e81457
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-24 14:07:08 +0100

    add images folder and e2e test for product count homepage

--- test/e2e/storeFeature.js
+++ test/e2e/storeFeature.js
@@ -1,8 +1,13 @@
 describe('Homepage', function() {
 
   browser.get('index.html');
+  productList = element.all(by.tagName('h5'));
 
   it('has a title', function() {
     expect(browser.getTitle()).toEqual('Clothes Store');
+  });
+
+  it('displays 13 products', function() {
+    expect(productList.count()).toBe(13);
   });
 });
