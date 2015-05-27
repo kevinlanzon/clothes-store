84df048076dbe1729d96dcb87dbec25b3381e223
Kevin Lanzon <kevinlanzon@gmail.com>
2015-05-27 14:31:21 +0100

    moving store html into a partial

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,9 @@
-var app = angular.module('clothesStore', []);
+var app = angular.module('clothesStore', ['ngRoute','clothesStore']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'partials/store.html',
+    controller: 'StoreController'
+  });
+}]);
